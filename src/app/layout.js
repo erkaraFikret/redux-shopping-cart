e04586b0e7df01ba3943c0ed5cd415d5fd82a8c0
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-01-23 14:55:25 +0300

    added daisy ui and created pages folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +22,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
-        {children}
+        <Navbar />
+        <main className="max-w-7xl mx-auto max-h-screen">{children}</main>
       </body>
     </html>
   );
